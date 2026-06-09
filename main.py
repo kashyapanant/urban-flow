0ca38b4b713b475d70a6bc756373ba7ee5c208c1
kashyapanant <anant.kashyap99@gmail.com>
2026-06-09 07:45:07 +0200

    fix(main): use full module path in uvicorn.run

--- main.py
+++ main.py
@@ -71,4 +71,6 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run(
+        "backend.api.main:app", host="0.0.0.0", port=8000, reload=True, log_level="info"
+    )
